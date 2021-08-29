94b06fd4fab189747f6be09320c68ab44250d52c
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2021-08-28 17:05:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    // Declaring constants
+    const slideshowContainer = document.getElementById("slideshowContainer");
+
+    const slides = document.getElementsByClassName("slideshowElement");
+
+    const dots = document.getElementsByClassName("dot");
+
+    const previousSlideArrow = document.getElementById("previousPicture");
+
+    const nextSlideArrow = document.getElementById("nextPicture");
+
+    function newSlide() {
+
+        
+
+    }
+
+
+    // On Load
+
+
+});
